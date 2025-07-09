f62960668409b7febef2fdaa3bacaaab7108c77e
jpswdeveloper <girmatedy48@gmail.com>
2025-07-09 15:01:43 +0300

    convert number types into String

--- models/SiteInfo.js
+++ models/SiteInfo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const siteInfoSchema = new mongoose.Schema({
-  siteId: { type: Number, required: true },
-  azimute: { type: Number, required: true },
-  electricalTilt: { type: Number, required: true },
-  mechanicalTilt: { type: Number, required: true },
-  antennaHeight: { type: Number, required: true },
+  siteId: { type: String, required: true },
+  azimute: { type: String, required: true },
+  electricalTilt: { type: String, required: true },
+  mechanicalTilt: { type: String, required: true },
+  antennaHeight: { type: String, required: true },
 });
 
 module.exports = mongoose.model('SiteInfo', siteInfoSchema); 
